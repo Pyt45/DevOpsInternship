671a3f91d40168119086a58e87f4e917ffb5ca3a
pyt45 <ayoub1234aqlzim@gmail.com>
2022-06-07 16:13:20 +0100

    refactor: refactor structure of code

---
+++ README.md
@@ -0,0 +1,8 @@
+# KOBY INFRASTRACTURE:
+## INTRODUCTION
+1. install vagrant
+## TODO
+1. setup a private docker repository for the application
+2. set up k8s cluster using kubeadm and vagrant to manage the lifecycle of vms
+3. set up the pipeline with jenkins and deploy it to k8s cluster
+4. test the ci/cd setup
---
+++ scripts/install_vagrant.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+os_type=$(uname -s) # ethier Darwin or Linux or Windows
+
+if [[ $os_type == "Darwin" ]]; then
+    # install vagrant
+    which brew &> /dev/null
+    if [ "$?" -eq 0 ]; then
+        brew install vagrant
+    else
+        sudo apt install vagrant
+    fi
+elif [[ $os_type == "Linux" ]]; then
+    os=$(grep "^NAME" /etc/os-release | cut -d'=' -f2)
+    if [[ $os == '"CentOS Linux"' ]]; then
+        sudo yum install -y yum-utils
+        sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+        sudo yum -y install vagrant
+    else [[ $os == '"Ubuntu"' ]]; then
+        curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+        sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+        sudo apt-get update && sudo apt-get install vagrant
+    else
+        echo "for other installation go a head to: "
+        echo "https://www.vagrantup.com/downloads"
+    fi
+fi
+
+echo "For windows installation go a head to: "
+echo "https://www.vagrantup.com/downloads"
