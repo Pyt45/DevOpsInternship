7e1bf54fa015016018a91c5acfde8b2de062cd0a
pyt45 <ayoub1234aqlzim@gmail.com>
2022-06-15 16:59:30 +0100

    progress

--- gitlab/gitlab-commands.sh
+++ gitlab/gitlab-commands.sh
@@ -2,6 +2,9 @@
 
 sudo curl -L --output /usr/local/bin/gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
 sudo chmod +x /usr/local/bin/gitlab-runner
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo /usr/local/bin/gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+sudo /usr/local/bin/gitlab-runner start
 
 kubectl create namespace gitlab
 sudo firewall-cmd --permanent --add-port=53/tcp
